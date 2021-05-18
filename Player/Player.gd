0d8db8d44cceb0cd9abb437f1167548015823870
SkyluxSky <skyluxsky@gmail.com>
2021-05-18 19:18:29 -0400

    Added Animations and Camera

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,12 +11,12 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 #default character speed
-const SPEED = 1000
+const SPEED = 1500
 #default GRAVITY
 const GRAVITY = 300
 #Defines up direction
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 3000
+const JUMP_SPEED = 5000
 
 #Function for player movement.
 func _physics_process(delta):
@@ -25,6 +25,7 @@ func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 	#print(delta)
 
@@ -36,7 +37,7 @@ func apply_gravity():
 		
 func jump():
 	#Define Jumping
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_pressed("Jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 	
 	
@@ -48,3 +49,20 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+		
+
+#animate character
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+		
+	elif motion.x > 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false#Face Right
+		
+	elif motion.x < 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true#Face Left
+		
+	else:
+		$AnimatedSprite.play("idle")
